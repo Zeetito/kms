bd5b6096b6024494ad95d97724d75dc2dbc74b1f
Zeetito <agyareernest44@gmail.com>
2024-08-15 14:31:31 +0000

    Register and Update User set

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api\V1;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -14,10 +15,40 @@ class UserController extends Controller
         $users = User::orderByDesc('status')->get();
         return response()->json($users);
     }
+    
+    // Register New User
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'othername' => 'nullable',
+            'gender' => 'required',
+            'is_member'=> 'required',
+            'email' => 'required|email|unique:users',
+            'is_alumni' => 'required',
+            'is_knust_affiliate' => 'required',
+            'status' => 'required',
+            'dob'  => 'required',
+            'password' => 'required',
+          
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $validator->errors(),
+                'status' => 'failure'
+            ], 422);
+        }
+        $user = User::create($request->all());
 
-    // Get all students
-    public function students(){
-        $students = User::students()->get();
-        return response()->json($students);
+        return response()->json($user);
     }
+
+    // Update User
+    public function update(Request $request, User $user){
+        // $user = User::find($id);
+        $user->update($request->all());
+        return response()->json($user);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -22,65 +22,77 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // USER
-    // Get all users
-    Route::get('/users', [UserController::class, 'index'])
-    ->middleware('auth:sanctum')
-    ;
-
-    // NON MEMBERS
-        // Get all alumni
-        Route::middleware('auth:sanctum')->get('/alumni', function () {
-            $users = User::members()->orderByDesc('status')->get();
-            return response()->json($users);
-        });
-    // MEMBERS
-        // Get all members
-        Route::middleware('auth:sanctum')->get('/members', function () {
-            $users = User::members()->orderByDesc('status')->get();
-            return response()->json($users);
-        });
-        // Get affliliate members
-        Route::middleware('auth:sanctum')->get('/affiliate_members', function () {
-            $users = User::affiliate_members()->orderByDesc('status')->get();
-            return response()->json($users);
-        });
-
-        // Get non affiliate members
-        Route::middleware('auth:sanctum')->get('/non_affiliate_members', function () {
-            $users = User::non_affiliate_members()->orderByDesc('status')->get();
-            return response()->json($users);
-        });
-
-        // Get alumni members
-        Route::middleware('auth:sanctum')->get('/alumni_members', function () {
-            $users = User::alumni_members()->orderByDesc('status')->get();
-            return response()->json($users);
-        });
-
-        // Workers Members
-        Route::middleware('auth:sanctum')->get('/workers_members', function () {
-            $users = User::workers_members()->orderByDesc('status')->get();
-            return response()->json($users);
-        });
-
-    
-    // STUDENTS
-        // Get all students
-        Route::get('/students', [UserController::class, 'students'])
+    // GET USER
+        // Get all users
+        Route::get('/users', [UserController::class, 'index'])
         ->middleware('auth:sanctum')
         ;
 
-        // Get all affiliate students
-        Route::middleware('auth:sanctum')->get('/affiliate_students', function () {
-            $users = User::affiliate_students()->orderByDesc('status')->get();
-            return response()->json($users);
-        });
+        // NON MEMBERS
+            // Get all alumni
+            Route::middleware('auth:sanctum')->get('/alumni', function () {
+                $users = User::members()->orderByDesc('status')->get();
+                return response()->json($users);
+            });
+        // MEMBERS
+            // Get all members
+            Route::middleware('auth:sanctum')->get('/members', function () {
+                $users = User::members()->orderByDesc('status')->get();
+                return response()->json($users);
+            });
+            // Get affliliate members
+            Route::middleware('auth:sanctum')->get('/affiliate_members', function () {
+                $users = User::affiliate_members()->orderByDesc('status')->get();
+                return response()->json($users);
+            });
 
-        // Get non affiliate students
-        Route::middleware('auth:sanctum')->get('/non_affiliate_students', function () {
-            $users = User::non_affiliate_students()->orderByDesc('status')->get();
-            return response()->json($users);
-        });
+            // Get non affiliate members
+            Route::middleware('auth:sanctum')->get('/non_affiliate_members', function () {
+                $users = User::non_affiliate_members()->orderByDesc('status')->get();
+                return response()->json($users);
+            });
+
+            // Get alumni members
+            Route::middleware('auth:sanctum')->get('/alumni_members', function () {
+                $users = User::alumni_members()->orderByDesc('status')->get();
+                return response()->json($users);
+            });
+
+            // Workers Members
+            Route::middleware('auth:sanctum')->get('/workers_members', function () {
+                $users = User::workers_members()->orderByDesc('status')->get();
+                return response()->json($users);
+            });
+
+        
+        // STUDENTS
+            // Get all students
+            Route::get('/students', [UserController::class, 'students'])
+            ->middleware('auth:sanctum')
+            ;
+
+            // Get all affiliate students
+            Route::middleware('auth:sanctum')->get('/affiliate_students', function () {
+                $users = User::affiliate_students()->orderByDesc('status')->get();
+                return response()->json($users);
+            });
+
+            // Get non affiliate students
+            Route::middleware('auth:sanctum')->get('/non_affiliate_students', function () {
+                $users = User::non_affiliate_students()->orderByDesc('status')->get();
+                return response()->json($users);
+            });
+
+    // POST USER
+        // Register
+        Route::post('/register', [UserController::class, 'register'])
+        ->middleware('auth:sanctum')
+        ;
+
+        // Update
+        Route::post('/update_user/{user}', [UserController::class, 'update'])
+        ->middleware('auth:sanctum')
+        ;
 
 // AUTH
     // Login
