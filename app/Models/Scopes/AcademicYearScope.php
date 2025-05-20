01affc83d05203eef4844a41ea687e7333652110
Zeetito <agyareernest44@gmail.com>
2025-05-20 13:35:12 +0000

    Updated Academic Year Scope

--- app/Models/Scopes/AcademicYearScope.php
+++ app/Models/Scopes/AcademicYearScope.php
@@ -3,6 +3,7 @@
 namespace App\Models\Scopes;
 
 use App\Models\User;
+use App\Models\Semester;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Builder;
@@ -15,6 +16,7 @@ class AcademicYearScope implements Scope
     public function apply(Builder $builder, Model $model): void
     {
         // $builder->where('academic_year_id', auth()->user()->user_semester()->academic_year_id);
-        $builder->where('academic_year_id', User::find(2)->user_semester()->academic_year_id);
+        // $builder->where('academic_year_id', User::find(2)->user_semester()->academic_year_id);
+        $builder->where('academic_year_id', Semester::active_semester()->academic_year_id);
     }
 }
